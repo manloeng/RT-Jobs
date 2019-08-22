cbb8c12c3b9475c4cbbc259f1ac337bcb3e4c770
madalinmonaco <madalinmonaco@gmail.com>
2019-08-22 16:17:42 +0100

    Got buttons and title work

--- RTJobs/Homepage.js
+++ RTJobs/Homepage.js
@@ -1,12 +1,33 @@
 import React from "react";
-import { Button } from "react-native";
+import { Text, Button, View } from "react-native";
+
 class Homepage extends React.Component {
   static navigationOptions = {
     title: "Welcome"
   };
   render() {
     // const { navigate } = this.props.navigation;
-    return <Button title="Anna" />;
+    return (
+      <View>
+        <Text>RT Jobs</Text>
+        <View>
+          <Button
+            title="Yes"
+            onPress={() => {
+              console.log("clicked");
+            }}
+          />
+        </View>
+        <View>
+          <Button
+            title="No"
+            onPress={() => {
+              console.log("clicked");
+            }}
+          />
+        </View>
+      </View>
+    );
   }
 }
 export default Homepage;
