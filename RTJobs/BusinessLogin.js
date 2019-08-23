0bfbd50e8b8314cdbb60301099b1eceadfb3c926
madalinmonaco <madalinmonaco@gmail.com>
2019-08-23 13:09:24 +0100

    Added email address and password + login and signup buttons on the Business Login page

--- RTJobs/BusinessLogin.js
+++ RTJobs/BusinessLogin.js
@@ -1,15 +1,27 @@
 import React from "react";
-import { StyleSheet, Text, Button, View } from "react-native";
-
+import { StyleSheet, Text, Button, View, ScrollView } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 class BusinessLogin extends React.Component {
+  static navigationOptions = {
+    title: "RT Jobs"
+  };
   render() {
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Business Login</Text>
-        <Text>Name</Text>
+        <Text style={{ fontSize: 20 }}>Business Login</Text>
+        <View>
+          <TextInput placeholder="Email Address" />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry={true}
+            password={true}
+          />
+          <View style={{ margin: 7 }} />
+          <Button onPress={this.props.onLoginPress} title="Login" />
+          <Button onPress={this.props.onLoginPress} title="Sign Up" />
+        </View>
       </View>
     );
   }
 }
-
 export default BusinessLogin;
