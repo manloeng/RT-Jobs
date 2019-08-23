197395cb4279c2251e2e51e3a0df0f62da77039c
madalinmonaco <madalinmonaco@gmail.com>
2019-08-23 15:41:05 +0100

    Added Business Sign Up page

--- RTJobs/App.js
+++ RTJobs/App.js
@@ -2,11 +2,13 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 import Homepage from "./Homepage";
 import BusinessLogin from "./BusinessLogin";
 import ApplicantLogin from "./ApplicantLogin";
+import BusinessSignup from "./BusinessSignup";
 
 const MainNavigator = createStackNavigator({
   Home: { screen: Homepage },
   BusinessLogin: { screen: BusinessLogin },
-  ApplicantLogin: { screen: ApplicantLogin }
+  ApplicantLogin: { screen: ApplicantLogin },
+  BusinessSignup: { screen: BusinessSignup }
 });
 
 const App = createAppContainer(MainNavigator);
