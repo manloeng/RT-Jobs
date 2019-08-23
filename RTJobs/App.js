e7fdf0605833d72497f3929244ccc05a4764fe3b
madalinmonaco <madalinmonaco@gmail.com>
2019-08-23 16:04:33 +0100

    Changed nav in app file

--- RTJobs/App.js
+++ RTJobs/App.js
@@ -2,13 +2,11 @@ import { createStackNavigator, createAppContainer } from "react-navigation";
 import Homepage from "./Homepage";
 import BusinessLogin from "./BusinessLogin";
 import ApplicantLogin from "./ApplicantLogin";
-import BusinessSignup from "./BusinessSignup";
 
 const MainNavigator = createStackNavigator({
   Home: { screen: Homepage },
   BusinessLogin: { screen: BusinessLogin },
-  ApplicantLogin: { screen: ApplicantLogin },
-  BusinessSignup: { screen: BusinessSignup }
+  ApplicantLogin: { screen: ApplicantLogin }
 });
 
 const App = createAppContainer(MainNavigator);
