cb682381bd10079392009e7f0930cdad7ce7956e
Achugs <anna_9400@hotmail.com>
2019-08-23 15:40:52 +0100

    applicant sign up page added

--- RTJobs/ApplicantSignUp.js
+++ RTJobs/ApplicantSignUp.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { StyleSheet, Text, Button, View, ScrollView } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 
 class ApplicantSignUp extends React.Component {
   static navigationOptions = {
     title: "RT Jobs"
   };
   render() {
-    const { navigate } = this.props.navigation;
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Text>Applicant SignUp</Text>
+        <Text style={{ fontSize: 20 }}>Applicant Sign Up</Text>
+        <View>
+          <TextInput placeholder="Applicant Name" />
+          <TextInput placeholder="Email Address" />
+          <TextInput
+            placeholder="Password"
+            secureTextEntry={true}
+            password={true}
+          />
+          <View style={{ margin: 7 }} />
+          <Button title="Sign Up" />
+        </View>
       </View>
     );
   }
 }
-
 export default ApplicantSignUp;
