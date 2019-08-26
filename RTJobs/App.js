866ab5bdb6e52f5ec4be9e569a77a2d3e5045ecf
achugs <48457377+achugs@users.noreply.github.com>
2019-08-26 11:26:02 +0100

    Useravailablejobs (#14)
    * navigating for list of jobs an applicnat can apply for complete
    
    * added applicant jobs applied to jobs
    
    * navigation to applicant jobs applied for complete
    
    * Update ApplicantLogin.js

--- RTJobs/App.js
+++ RTJobs/App.js
@@ -4,13 +4,17 @@ import BusinessLogin from "./BusinessLogin";
 import ApplicantLogin from "./ApplicantLogin";
 import ApplicantSignUp from "./ApplicantSignUp";
 import BusinessSignup from "./BusinessSignup";
+import ApplicantAvailableJobs from "./ApplicantAvailableJobs";
+import ApplicantJobsApplied from "./ApplicantJobsApplied";
 
 const MainNavigator = createStackNavigator({
   Home: { screen: Homepage },
   BusinessLogin: { screen: BusinessLogin },
   ApplicantLogin: { screen: ApplicantLogin },
   ApplicantSignUp: { screen: ApplicantSignUp },
-  BusinessSignup: { screen: BusinessSignup }
+  BusinessSignup: { screen: BusinessSignup },
+  ApplicantAvailableJobs: { screen: ApplicantAvailableJobs },
+  ApplicantJobsApplied: { screen: ApplicantJobsApplied }
 });
 
 const App = createAppContainer(MainNavigator);
