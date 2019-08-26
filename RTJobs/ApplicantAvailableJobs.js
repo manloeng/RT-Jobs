a8441ac412a68d28fdf5a7b8e99e7f9b491298af
madalin <46004045+madalinmonaco@users.noreply.github.com>
2019-08-26 13:21:27 +0100

    Added everything from fecomponent to the new branch (#16)

--- RTJobs/ApplicantAvailableJobs.js
+++ RTJobs/ApplicantAvailableJobs.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { StyleSheet, Text, Button, View, ScrollView } from "react-native";
+import { Text, Button, View, TouchableOpacity } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
-
 class ApplicantAvailableJobs extends React.Component {
   static navigationOptions = {
     title: "RT Jobs"
@@ -11,14 +10,15 @@ class ApplicantAvailableJobs extends React.Component {
     const { navigate } = this.props.navigation;
     return (
       <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-        <Button
-          title="Your jobs"
+        <TouchableOpacity
           onPress={() =>
             navigate("ApplicantJobsApplied", {
               name: "ApplicantJobsApplied"
             })
           }
-        />
+        >
+          <Text>Your Jobs</Text>
+        </TouchableOpacity>
         <Text style={{ fontSize: 20 }}>
           List of jobs an applicant can apply for goes here
         </Text>
--- RTJobs/ApplicantSignUp.js
+++ RTJobs/ApplicantSignUp.js
@@ -1,5 +1,12 @@
 import React from "react";
-import { StyleSheet, Text, Button, View, ScrollView } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  Button,
+  View,
+  ScrollView,
+  TouchableOpacity
+} from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 import * as api from "./api";
 
@@ -42,15 +49,16 @@ class ApplicantSignup extends React.Component {
             password={true}
           />
           <View style={{ margin: 7 }} />
-          <Button
+          <TouchableOpacity
             onChange={this.handleTextChange}
             type="submit"
             value="Submit"
-            title="Sign Up"
             onPress={e => {
               this.handleSubmit(e);
             }}
-          />
+          >
+            <Text>Sign Up</Text>
+          </TouchableOpacity>
         </View>
       </View>
     );
