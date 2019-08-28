61617c8a3a89dd11698dfe763094727691a439c1
Andrew Chung <37455284+manloeng@users.noreply.github.com>
2019-08-28 15:54:06 +0100

    Revert "Styling (#24)"
    This reverts commit 67c2557ad42509b4bfa75f305351995b20513977.

--- RTJobs/BusinessApplicantList.js
+++ RTJobs/BusinessApplicantList.js
@@ -7,61 +7,10 @@ import {
   ScrollView,
   TouchableOpacity
 } from "react-native";
+import { TextInput } from "react-native-gesture-handler";
 import * as api from "./api";
 
-const styles = StyleSheet.create({
-  baseText: {
-    fontFamily: "Roboto"
-  },
-  title: {
-    fontSize: 50,
-    color: '#F5F5EF',
-    fontWeight: "bold",
-  },
-    container: {
-    flexGrow: 1,
-    alignItems: "center",
-    backgroundColor: '#047884'
-  },
-  button: {
-    backgroundColor: "#F5F5EF",
-    padding: 10,
-    borderRadius: 5,
-    borderRadius: 40,
-    borderColor: "#303838",
-    borderWidth: 1,
-    margin: 4,
-    width: 150,
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  text: {
-    color: "#303838",
-    fontWeight: "bold",
-    fontSize: 20,
-  },
-  subContainer:{
-    backgroundColor: '#F5F5EF',
-    borderRadius: 25,
-    borderWidth: 1,
-    borderColor: 'white',
-    margin: 10,
-    padding: 10
-  },
-  applcaitionContainer:{
-    backgroundColor: '#F5F5EF',
-    width: 350,
-    borderRadius: 25,
-    borderWidth: 1,
-    margin: 10,
-    padding: 10
-  }
-});
-
 class BusinessApplicantList extends React.Component {
-  static navigationOptions = {
-    title: "Job Details"
-  };
   state = {
     job: {},
     applicants: []
@@ -83,66 +32,51 @@ class BusinessApplicantList extends React.Component {
     } = this.state.job;
     const { applicants } = this.state;
     return (
-      <ScrollView contentContainerStyle={styles.container}>
-        <View >
-          <View style={styles.subContainer}>
-            {/* <Text style={styles.text}>Job Details:</Text> */}
-            <Text style={styles.text}>{created_by}</Text>
-
-            <Text style={{  color: "#303838", fontWeight: "bold"}}>Job Description:</Text>
-            <Text>{description}{"\n"}</Text>
-            
-            <Text>Position: {title}</Text>
-            <Text>Start Date: {date}</Text>
-            <Text>Location: {location}</Text>
-            <Text>Vacancies: {vacancies}</Text>
-            <Text>pph:{pay}</Text>
-          </View>
-          <Text style={[styles.title, {marginLeft: 40}]}>Applications</Text>
-          {applicants.map(applicant => {
-            console.log(applicant, "here");
-            return (
-              <View style={styles.applcaitionContainer}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                  <View style={{alignItems: 'center', width:150, justifyContent: 'center'}}> 
-                    <Text style={styles.text}>{applicant.display_name}</Text>
-                  </View>
-                  <TouchableOpacity onPress={() => {
+      <View>
+        <Text>Job Details</Text>
+        <View>
+          <Text>{created_by}</Text>
+          <Text>{title}</Text>
+          <Text>{date}</Text>
+          <Text>{location}</Text>
+          <Text>{vacancies}</Text>
+          <Text>{pay}</Text>
+          <Text>{description}</Text>
+        </View>
+        <Text>Applications</Text>
+        {applicants.map(applicant => {
+          console.log(applicant, "here");
+          return (
+            <View>
+              <Text>{applicant.display_name}</Text>
+              <TouchableOpacity
+                onPress={() => {
+                  api.postBusinessApproval(applicant.applications, "Approve");
+                }}
+              >
+                <Text>Confirm</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                onPress={() => {
+                  api.postBusinessApproval(applicant.applications, "Reject");
+                }}
+              >
+                <Text>Reject</Text>
+              </TouchableOpacity>
+              <TouchableOpacity onPress={() => {
                 this.props.navigation.navigate("Chat", {
                   display_name: applicant.display_name,
                   created_by: applicant.created_by,
                   business: true,
                   token
                 })
-              }}
-              style={[styles.button, {backgroundColor: '#af96ca'}]}>
-                    <Text>Contact</Text>
-                  </TouchableOpacity>
+              }}>
                 <Text>Contact</Text>
               </TouchableOpacity>
-                </View>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                  <TouchableOpacity
-                  style={[styles.button, {backgroundColor: '#D2F2A6'}]}
-                    onPress={() => {
-                      api.postBusinessApproval(applicant.applications, "Approve");
-                    }}
-                  >
-                    <Text>Confirm</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity
-                    style={[styles.button, {backgroundColor: '#F5A758'}]}
-                    onPress={() => {
-                      api.postBusinessApproval(applicant.applications, "Reject");
-                    }}
-                  >
-                    <Text>Reject</Text>
-                  </TouchableOpacity>           
-                </View>
-              </View>
-            );
-          })}
-        </View>
+            </View>
+          );
+        })}
+      </View>
     );
   }
   componentDidMount() {
