7f4d7c1af7e6944a322f9cb1cfd9ecbd9456606f
madalin <46004045+madalinmonaco@users.noreply.github.com>
2019-08-28 17:37:51 +0100

    Added styling on the business applications page (#31)

--- RTJobs/BusinessApplicantList.js
+++ RTJobs/BusinessApplicantList.js
@@ -15,13 +15,13 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 50,
-    color: '#F5F5EF',
-    fontWeight: "bold",
+    color: "#F5F5EF",
+    fontWeight: "bold"
   },
-    container: {
+  container: {
     flexGrow: 1,
     alignItems: "center",
-    backgroundColor: '#047884'
+    backgroundColor: "#047B84"
   },
   button: {
     backgroundColor: "#F5F5EF",
@@ -33,28 +33,51 @@ const styles = StyleSheet.create({
     margin: 4,
     width: 150,
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "center"
   },
   text: {
     color: "#303838",
     fontWeight: "bold",
-    fontSize: 20,
+    fontSize: 20
   },
-  subContainer:{
-    backgroundColor: '#F5F5EF',
+  subContainer: {
+    backgroundColor: "#F5F5EF",
     borderRadius: 25,
     borderWidth: 1,
-    borderColor: 'white',
+    borderColor: "white",
     margin: 10,
     padding: 10
   },
-  applcaitionContainer:{
-    backgroundColor: '#F5F5EF',
+  applcaitionContainer: {
+    backgroundColor: "#F5F5EF",
     width: 350,
     borderRadius: 25,
     borderWidth: 1,
     margin: 10,
     padding: 10
+  },
+  buttonText: {
+    color: "#303838",
+    fontWeight: "bold",
+    fontSize: 15
+  },
+  cardText: {
+    fontSize: 15,
+    color: "#303838",
+    alignSelf: "center",
+    padding: 2
+  },
+  jobTitleText: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "#303838",
+    alignSelf: "center",
+    padding: 2
+  },
+  detailText: {
+    fontSize: 15,
+    color: "#303838",
+    padding: 2
   }
 });
 
@@ -84,57 +107,99 @@ class BusinessApplicantList extends React.Component {
     const { applicants } = this.state;
     return (
       <ScrollView contentContainerStyle={styles.container}>
-        <View >
+        <View>
           <View style={styles.subContainer}>
-            <Text style={styles.text}>{created_by}</Text>
-
-            <Text 
-            onPress={() => {
-                this.props.navigation.navigate("Chat", {
-                  display_name: applicant.display_name,
-                  created_by: applicant.created_by,
-                  business: true,
-                  token: applicant.token
-                })
-              }} 
-              style={{  color: "#303838", fontWeight: "bold"}}>Job Description:</Text>
-            <Text>{description}{"\n"}</Text>
-            
-            <Text>Position: {title}</Text>
-            <Text>Start Date: {date}</Text>
-            <Text>Location: {location}</Text>
-            <Text>Vacancies: {vacancies}</Text>
-            <Text>pph:{pay}</Text>
+            <Text style={styles.jobTitleText}>{title} </Text>
+            <Text style={styles.cardText}>{created_by}</Text>
+            <View
+              style={{
+                flexDirection: "row",
+                justifyContent: "space-around"
+              }}
+            >
+              <View>
+                <Text style={styles.detailText}>Start Date: {date} </Text>
+                <Text style={styles.detailText}>Start Time: {start_time} </Text>
+              </View>
+              <View>
+                <Text style={styles.detailText}>Pay: {pay} </Text>
+                <Text style={styles.detailText}>Duration: {duration} </Text>
+              </View>
+            </View>
+            <View
+              style={{
+                borderWidth: 1,
+                margin: 5,
+                borderRadius: 10,
+                padding: 5
+              }}
+            >
+              <Text style={styles.cardText}>Job Description</Text>
+              <Text style={styles.cardText}>{description} </Text>
+            </View>
+            <View style={{ alignSelf: "center" }}>
+              <Text style={styles.cardText}>Location: {location}</Text>
+            </View>
           </View>
-          <Text style={[styles.title, {marginLeft: 40}]}>Applications</Text>
+          <Text style={[styles.title, { marginLeft: 40 }]}>Applications</Text>
           {applicants.map(applicant => {
             console.log(applicant, "here");
             return (
               <View style={styles.applcaitionContainer}>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                  <View style={{alignItems: 'center', width:150, justifyContent: 'center'}}> 
-                    <Text style={styles.text}>{applicant.display_name}</Text>
-                  </View>
-                  <TouchableOpacity style={[styles.button, {backgroundColor: '#af96ca'}]}>
-                    <Text>Contact</Text>
-                  </TouchableOpacity>
-                </View>
-                <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-                  <TouchableOpacity
-                  style={[styles.button, {backgroundColor: '#D2F2A6'}]}
-                    onPress={() => {
-                      api.postBusinessApproval(applicant.applications, "accepted");
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between"
+                  }}
+                >
+                  <View
+                    style={{
+                      alignItems: "center",
+                      width: 150,
+                      justifyContent: "center"
                     }}
                   >
-                    <Text>Confirm</Text>
+                    <Text style={styles.text}>{applicant.display_name}</Text>
+                  </View>
+                  <TouchableOpacity
+                    onPress={() => {
+                      this.props.navigation.navigate("Chat", {
+                        display_name: applicant.display_name,
+                        created_by: applicant.created_by,
+                        business: true,
+                        token: applicant.token
+                      });
+                    }}
+                    style={[styles.button, { backgroundColor: "#af96ca" }]}
+                  >
+                    <Text style={styles.buttonText}>Contact</Text>
+                  </TouchableOpacity>
+                </View>
+                <View
+                  style={{
+                    flexDirection: "row",
+                    justifyContent: "space-between"
+                  }}
+                >
+                  <TouchableOpacity
+                    style={[styles.button, { backgroundColor: "#D2F2A6" }]}
+                    onPress={() => {
+                      api.postBusinessApproval(applicant.applications, "offer");
+                    }}
+                  >
+                    <Text style={styles.buttonText}>Confirm</Text>
                   </TouchableOpacity>
                   <TouchableOpacity
-                    style={[styles.button, {backgroundColor: '#F5A758'}]}
+                    style={[styles.button, { backgroundColor: "#F5A758" }]}
                     onPress={() => {
-                      api.postBusinessApproval(applicant.applications, "rejected")}}
+                      api.postBusinessApproval(
+                        applicant.applications,
+                        "rejected"
+                      );
+                    }}
                   >
-                    <Text>Reject</Text>
-                  </TouchableOpacity>           
+                    <Text style={styles.buttonText}>Reject</Text>
+                  </TouchableOpacity>
                 </View>
               </View>
             );
@@ -154,4 +219,3 @@ class BusinessApplicantList extends React.Component {
   }
 }
 export default BusinessApplicantList;
-
